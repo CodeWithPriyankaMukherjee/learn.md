acf1ce1946945d32e09e9e600f9a236751c848b8
Priyanka Mukherjee <priyaisamukherjee524@gmail.com>
2026-08-09 11:07:25 +0530

    Update firebase.js

--- learn-md/public/os-quiz/firebase.js
+++ learn-md/public/os-quiz/firebase.js
@@ -6,14 +6,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_FIREBASE_AUTH_DOMAIN",
-  projectId: "YOUR_FIREBASE_PROJECT_ID",
-  storageBucket: "YOUR_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "YOUR_FIREBASE_APP_ID",
+  apiKey: "AIzaSyCkbDbr1QL-9lI4WcYZJPaTiPlNjlvyzxs",
+  authDomain: "learn-md-c0119.firebaseapp.com",
+  projectId: "learn-md-c0119",
+  storageBucket: "learn-md-c0119.firebasestorage.app",
+  messagingSenderId: "945956164832",
+  appId: "1:945956164832:web:c9d62497e29316f98c2c85"
 };
-
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
