f7c9615dbbe7337598f6cc6becd2c1ba4d2d39fe
Priyanka Mukherjee <priyaisamukherjee524@gmail.com>
2026-08-09 11:21:27 +0530

    Create teacherService.js

---
+++ learn-md/public/teacher-dashboard/teacherService.js
@@ -0,0 +1,22 @@
+import {
+  collection,
+  getDocs,
+  query,
+  orderBy,
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+import { db } from "./firebase.js";
+
+export async function getQuizAttempts() {
+  const q = query(
+    collection(db, "quizAttempts"),
+    orderBy("completedAt", "desc")
+  );
+
+  const snapshot = await getDocs(q);
+
+  return snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+}
